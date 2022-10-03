510c70ce89daa268431f1673b9905c6a0e8a4cb9
Hyejeong Lee <skrau9993@gmail.com>
2022-10-03 19:49:22 +0900

    hj 10/03 commit

--- KIEE_KNN.py
+++ KIEE_KNN.py
@@ -5,20 +5,23 @@ import pandas as pd
 data_g=pd.read_excel('C:/Users/user/Desktop/psl/fault_feature_jirak.xls')
 data_l=pd.read_excel('C:/Users/user/Desktop/psl/fault_feature_seongan.xls')
 data_n=pd.read_excel('C:/Users/user/Desktop/psl/feature_normal.xls')
+data_p=pd.read_excel('C:/Users/user/Desktop/psl/fault_feature 3L_100.xls')
 
-df = pd.concat([data_g, data_l, data_n])
+df = pd.concat([data_g, data_l, data_n,data_p])
 
 print(df)
 
 target = []
 
 for idx, row in df.iterrows():
-    if row['type'] == 0.0:
+    if row['type'] == 0:
         target.append(0)
-    elif row['type'] == 1.0 or row['type'] == 2.0 or row['type'] == 3.0:
+    elif row['type'] == 1 or row['type'] == 2 or row['type'] == 3:
         target.append(1)
-    else:
+    elif row['type'] == 4 or row['type'] == 5 or row['type'] == 6:
         target.append(2)
+    else:
+        target.append(3)
 
 df['type'] = target
 
